307601966d31f0d427e113700d8b499936dd2a4c
Paul M. Christian <paulmchristian@gmail.com>
2016-08-26 17:00:43 -0500

    add map of glyphs and try some animation and drawing many items

--- src/cljs/cljs_musical/core.cljs
+++ src/cljs/cljs_musical/core.cljs
@@ -1,18 +1,36 @@
 (ns cljs-musical.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]))
+  (:require [reagent.core :as r :refer [atom]]
+            [reagent.session :as session]
+            [secretary.core :as secretary :include-macros true]
+            [accountant.core :as accountant]
+            [clojure.string :as string])
+  (:use [cljs-musical.glyphs :only [glyphs]]))
 
-;; -------------------------
-;; Views
+
+(def appstate (r/atom {:t 0}))
+
+(defn loopy [t] (mod (inc t) 10))
+
+(def jsinterval
+  (js/setInterval #(swap! appstate assoc :t (loopy (:t @appstate))) 500))
+
+(defn clearinterval [] (js/clearInterval jsinterval))
+
+(defn glyph [g pos] [:a-entity {:text     (str "text: " (g glyphs) "; font: Bravura")
+                            :position (string/join " " pos)
+                            :material "color: #000"}])
+
+(defn staff [n] (->> (range 0 n) (map (fn [i] (glyph :staff-5 [i 0 -10])))))
 
 (defn home-page []
   [:a-scene
-   [:a-entity {:text "text: \uE050; font: Bravura"
-               :position "0 0 -5"
-               :material "color: #66E1B4"}]
-   [:a-entity {:text "text: \uE1D9; font: Bravura"
+   (staff 10)
+   (glyph :staff-5 [0 0 -5])
+   [:a-entity {:text     (str "text: " (:t @appstate))
+               :position "-1 0 -5"
+               :abcd ""
+               :material "color: #544"}]
+   [:a-entity {:text     "text: \uE1D9; font: Bravura"
                :position "1 0 -5"
                :material "color: #544"}]
    ])
@@ -28,16 +46,16 @@
 ;; Routes
 
 (secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
+                    (session/put! :current-page #'home-page))
 
 (secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
+                    (session/put! :current-page #'about-page))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
+  (r/render [current-page] (.getElementById js/document "app")))
 
 (defn init! []
   (accountant/configure-navigation!
