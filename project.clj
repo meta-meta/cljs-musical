4bd82868d66e53a97bffadabb8383775fe6da3f4
Paul M. Christian <paulmchristian@gmail.com>
2016-08-29 01:03:03 -0500

    add prismatic/schema dep; formatting

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring-server "0.4.0"]
@@ -17,6 +17,7 @@
                  [org.clojure/clojurescript "1.9.93"
                   :scope "provided"]
                  [secretary "1.2.3"]
+                 [prismatic/schema "1.1.3"]
                  [venantius/accountant "0.1.7"
                   :exclusions [org.clojure/tools.reader]]]
 
@@ -25,7 +26,7 @@
             [lein-asset-minifier "0.2.7"
              :exclusions [org.clojure/clojure]]]
 
-  :ring {:handler cljs-musical.handler/app
+  :ring {:handler      cljs-musical.handler/app
          :uberwar-name "cljs-musical.war"}
 
   :min-lein-version "2.5.0"
@@ -35,9 +36,9 @@
   :main cljs-musical.server
 
   :clean-targets ^{:protect false}
-  [:target-path
-   [:cljsbuild :builds :app :compiler :output-dir]
-   [:cljsbuild :builds :app :compiler :output-to]]
+[:target-path
+ [:cljsbuild :builds :app :compiler :output-dir]
+ [:cljsbuild :builds :app :compiler :output-to]]
 
   :source-paths ["src/clj" "src/cljc"]
   :resource-paths ["resources" "target/cljsbuild"]
@@ -50,22 +51,20 @@
   {:builds {:min
             {:source-paths ["src/cljs" "src/cljc" "env/prod/cljs"]
              :compiler
-             {:output-to "target/cljsbuild/public/js/app.js"
-              :output-dir "target/uberjar"
-              :optimizations :advanced
-              :pretty-print  false}}
+                           {:output-to     "target/cljsbuild/public/js/app.js"
+                            :output-dir    "target/uberjar"
+                            :optimizations :advanced
+                            :pretty-print  false}}
             :app
             {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
              :compiler
-             {:main "cljs-musical.dev"
-              :asset-path "/js/out"
-              :output-to "target/cljsbuild/public/js/app.js"
-              :output-dir "target/cljsbuild/public/js/out"
-              :source-map true
-              :optimizations :none
-              :pretty-print  true}}
-
-
+                           {:main          "cljs-musical.dev"
+                            :asset-path    "/js/out"
+                            :output-to     "target/cljsbuild/public/js/app.js"
+                            :output-dir    "target/cljsbuild/public/js/out"
+                            :source-map    true
+                            :optimizations :none
+                            :pretty-print  true}}
 
             }
    }
@@ -73,38 +72,41 @@
 
   :figwheel
   {:http-server-root "public"
-   :server-port 3449
-   :nrepl-port 7002
+   :server-port      3449
+   :nrepl-port       7002
    :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
                       ]
-   :css-dirs ["resources/public/css"]
-   :ring-handler cljs-musical.handler/app}
+   :css-dirs         ["resources/public/css"]
+   :ring-handler     cljs-musical.handler/app}
 
 
+  :profiles {:dev     {:repl-options {:init-ns cljs-musical.repl}
 
-  :profiles {:dev {:repl-options {:init-ns cljs-musical.repl}
+                       :dependencies [[ring/ring-mock "0.3.0"]
+                                      [ring/ring-devel "1.5.0"]
+                                      [prone "1.1.1"]
+                                      [figwheel-sidecar "0.5.4-5"]
+                                      [org.clojure/tools.nrepl "0.2.12"]
+                                      [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
+                                      [pjstadig/humane-test-output "0.8.0"]
+                                      ]
 
-                   :dependencies [[ring/ring-mock "0.3.0"]
-                                  [ring/ring-devel "1.5.0"]
-                                  [prone "1.1.1"]
-                                  [figwheel-sidecar "0.5.4-5"]
-                                  [org.clojure/tools.nrepl "0.2.12"]
-                                  [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
-                                  [pjstadig/humane-test-output "0.8.0"]
-                                  ]
+                       ;; if you want to do REPL based development and not have
+                       ;; have compiled files autoloaded into the client env
+                       :autoload     false
 
-                   :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.4-5"]
-                             ]
+                       :source-paths ["env/dev/clj"]
+                       :plugins      [[lein-figwheel "0.5.4-5"]
+                                      ]
 
-                   :injections [(require 'pjstadig.humane-test-output)
-                                (pjstadig.humane-test-output/activate!)]
+                       :injections   [(require 'pjstadig.humane-test-output)
+                                      (pjstadig.humane-test-output/activate!)]
 
-                   :env {:dev true}}
+                       :env          {:dev true}}
 
-             :uberjar {:hooks [minify-assets.plugin/hooks]
+             :uberjar {:hooks        [minify-assets.plugin/hooks]
                        :source-paths ["env/prod/clj"]
-                       :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
-                       :env {:production true}
-                       :aot :all
-                       :omit-source true}})
+                       :prep-tasks   ["compile" ["cljsbuild" "once" "min"]]
+                       :env          {:production true}
+                       :aot          :all
+                       :omit-source  true}})
